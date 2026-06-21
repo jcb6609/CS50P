39c12b498ee261f9a05c08f33e3e840e7a4c170f
Julio Cesar Bermudez Viatela <jcb6609@psu.edu>
2026-06-21 00:06:24 -0500

    Update mario_func_abs.py

--- Week 2 - Loops/mario_func_abs.py
+++ Week 2 - Loops/mario_func_abs.py
@@ -1,5 +1,7 @@
 # If you like abstraction, when implementing print_square, we don't really care what it means to print a row (print_row),
 # we just need to know that someone's taking care of printing the row, you can pass the buck to another function altogether
+# and how does print_row work? It could use a for loop, it could use a string multiplication trick, etc.
+# This is a way to take a larger program- and to decompose it into these smaller components, that one assembled, achieve your final idea 
 
 def main():
     print_square(3)
