1e8e6a54490da7f38e6db608459db4c8d65f7140
Julio Cesar Bermudez Viatela <juliocbv2026@gmail.com>
2026-07-23 11:16:14 -0400

    Add files via upload

---
+++ Week 4 - Libraries/Shorts_W4/Style/students.py
@@ -0,0 +1,10 @@
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+    "Padma": "Ravenclaw"
+}
+
+for i in students:
+    print(i)
