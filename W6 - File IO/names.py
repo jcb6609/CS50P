ca02fb68194000bc571b396da8493e82e423cea7
Julio Cesar Bermudez Viatela <juliocbv2026@gmail.com>
2026-07-31 20:39:22 -0400

    .

--- W6 - File IO/names.py
+++
@@ -1,7 +0,0 @@
-names = [] # emtpy list
-
-for _ in range(3):
-    names.append(input("What is your name? "))
-
-for i in sorted(names): # the sorted() function goes through a sorted version of the 'names' list
-    print(f"hello, {i}")
